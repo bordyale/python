67f84fc926c1f19d43216bb015307f350e22bc09
bordyale <alessio.bordignon@gmail.com>
2025-01-20 13:43:51 +0100

    filter on created_stamp

--- vvordnot/test.py
+++ vvordnot/test.py
@@ -1,18 +1,2 @@
-#!/usr/bin/env python
-# modified from http://elinux.org/RPi_Email_IP_On_Boot_Debian
-import datetime
-import smtplib
-from email.mime.text import MIMEText
-from lib.loadprops import *
-
-
-
-# import urllib2
-
-
-    
-configs = loadprops()
-    
-today = datetime.date.today()
-subject = configs.get("passed_subject").data
-print(subject)
+msg: str ='Michael,Alessio:Marco;Nicola,Micro'
+print(msg.replace(';',',').replace(':',',').split(','))
